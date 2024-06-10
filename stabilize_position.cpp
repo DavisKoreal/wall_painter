89e9375de2ffd5cd5c5c418c8c339962b1525449
DavisKoreal <daviskipchirchir19@gmail.com>
2024-06-10 06:04:37 +0300

    The price for a more intelligent operation is an increase in computational complexity, energy requirements. A small price to pay.

---
+++ stabilize_position.cpp
@@ -0,0 +1,21 @@
+#include "wall_painter.hpp"
+
+
+/*
+Takes x,y,z coordinates and reactively stabilizes the drone in these coordinates
+
+THis coordinate is for painting on teh wall so the z ccordinate is a slight distance from the wall
+
+@param x: x coordinate on the wall
+@param y: y coordinate on the wall
+@param z: z coordinate on the wall
+@return bool: true if the position is reached, false if the position is not reached
+
+
+*/
+bool wallpainter::stabilize_position(int x, int y, int z){
+    // move to the position (x,y,z) on the wall
+    // return true if the position is reached
+    // return false if the position is not reached
+    return true;
+}
