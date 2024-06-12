73943679c5c3518e7db5a6b328144753d80e54a2
DavisKoreal <daviskipchirchir19@gmail.com>
2024-06-12 06:07:30 +0300

    MOre human touches to the agent

---
+++ take_picture.cpp
@@ -0,0 +1,9 @@
+#include "wall_painter.hpp"
+
+int wallpainter::take_picture(){
+    //take a picture of the wall
+    //Also take some scenic picures
+
+    
+    return 0;
+}
--- wall_painter.hpp
+++ wall_painter.hpp
@@ -11,6 +11,7 @@ class wallpainter{
         int map_wallpaper_to_wall();
         void stabilize_position(int x, int y, int z);
         bool move_to_position(int x, int y, int z);
+        int take_picture();
 }; 
 
 #endif
