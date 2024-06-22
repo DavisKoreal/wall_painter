1c7b6bd2b4e1f255a142dc76a121e16a917de2d7
DavisKoreal <daviskipchirchir19@gmail.com>
2024-06-23 02:33:17 +0300

    reactive positional correction. Now need a positioning system

--- paint.cpp
+++ paint.cpp
@@ -8,11 +8,11 @@ int wallpainter::paint(){
 
         //move to specific (x,y,z) position on wall
         move_to_position(wall_coordinates[x].x, wall_coordinates[x].y, wall_coordinates[x].z);
-        //get colour mapping to point (x,y) on wall
+        //get colour mapping to point (x,y) on wall and choose colour of paint depending on mapping colour
 
-        //choose colour of paint depending on mapping colour
-
+        
         //splutter the colour on the ccoordinates (x,y) on wall
+        
 
         //stabilize position(x,y,z) This should be asynchronous
         //further work will be done to always run an asynchoronus thread to stabilize position
--- stabilize_position.cpp
+++ stabilize_position.cpp
@@ -16,6 +16,18 @@ THis coordinate is for painting on teh wall so the z ccordinate is a slight dist
 void wallpainter::stabilize_position(int x, int y, int z){
     // move to the position (x,y,z) on the wall
     // return true if the position is reached
+    while(true){
+        coordinate current = get_current_coordinates();
+        double error_x = find_error(current.x, x);
+        double error_y = find_error(current.y, y);
+        double error_z = find_error(current.z, z);
+        move_to_position(x, y, z);
+    }
+
     
     // return false if the position is not reached
+}
+
+double find_error(double current, double target){
+    return target - current;
 }
